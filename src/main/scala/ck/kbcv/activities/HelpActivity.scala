a2795ecc0308cf9ffdc25c7d8315181010407de3
Christina <Christina.Kohl@student.uibk.ac.at>
2017-03-02 10:45:18 +0100

    help stub

--- src/main/scala/ck/kbcv/activities/HelpActivity.scala
+++ src/main/scala/ck/kbcv/activities/HelpActivity.scala
@@ -1,8 +1,31 @@
 package ck.kbcv.activities
 
-import ck.kbcv.TypedFindView
+import android.app.Fragment
+import android.os.Bundle
+import ck.kbcv.{R, TypedFindView}
 
 class HelpActivity extends NavigationDrawerActivity with TypedFindView {
+
+  override def onCreate( savedInstanceState: Bundle ): Unit = {
+    super.onCreate( savedInstanceState )
+
+    setContentView( R.layout.help_acitivity)
+   // val equationEditorCard = findView(TR.equation_editor_card)
+//    equationEditorCard.setOnClickListener(new OnClickListener {
+//      override def onClick(view: View): Unit = {
+//        val fragment = new HelpEquationEditor
+//        getSupportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).addToBackStack(null).commit()
+//        drawerToggle.setDrawerIndicatorEnabled(false)
+//
+//      }
+//    })
+
+  }
+
+  def switchFragment(fragment: Fragment): Unit = {
+    drawerToggle.setDrawerIndicatorEnabled(false)
+
+  }
 
 
 }
---
+++ src/main/scala/ck/kbcv/fragments/HelpPrecedence.scala
@@ -0,0 +1,29 @@
+package ck.kbcv.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.view.{LayoutInflater, MenuItem, View, ViewGroup}
+import ck.kbcv.R
+
+
+class HelpPrecedence extends Fragment {
+
+  override def onCreateView( inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle ): View = {
+    val view = inflater.inflate( R.layout.help_equation_editor, container, false )
+
+    setHasOptionsMenu(true)
+    getActivity.asInstanceOf[AppCompatActivity].getSupportActionBar.setDisplayHomeAsUpEnabled(true)
+    return view
+  }
+
+  override def onOptionsItemSelected(item: MenuItem): Boolean = {
+    if(item.getItemId == android.R.id.home) {
+      getActivity().onBackPressed()
+      true
+    } else super.onOptionsItemSelected(item)
+  }
+
+
+
+}
